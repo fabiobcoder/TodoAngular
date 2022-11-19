afe4521eb6cd025b81a71e3530a3965e19185b4f
FABIO BARBOSA <31438903+fabiobcoder@users.noreply.github.com>
2022-11-19 16:30:18 -0300

    Conclusão do Formulario do Todo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { Todo } from 'src/models/todo.model';
 
 @Component({
@@ -9,11 +10,37 @@ import { Todo } from 'src/models/todo.model';
 export class AppComponent {
   public todos: Todo [] = [];
   public title: String = 'Minhas Tarefas';
+  public form: FormGroup;
 
-  constructor() {
-    this.todos.push(new Todo(1, 'Passear com o Cachorro', false));
-    this.todos.push(new Todo(2, 'Supermercado', false));
-    this.todos.push(new Todo(3, 'Cortar o Cabelo', true));
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      title: ['', Validators.compose([
+        Validators.minLength(3),
+        Validators.maxLength(20),
+        Validators.required
+      ])]
+
+    });
+
+  this.load();
+
+    //this.todos.push(new Todo(1, 'Passear com o Cachorro', false));
+    //this.todos.push(new Todo(2, 'Supermercado', false));
+    //this.todos.push(new Todo(3, 'Cortar o Cabelo', true));
+
+  }
+
+  add(){
+    const title = this.form.controls['title'].value;
+    const id = this.todos.length + 1;
+    this.todos.push(new Todo(id, title, false));
+    this.save();
+    this.clear();
+  }
+
+  clear(){
+    this.form.reset();
   }
 
   remove(todo: Todo){
@@ -21,9 +48,26 @@ export class AppComponent {
     if(index !== -1) {
       this.todos.splice(index, 1);
     }
+    this.save();
   }
 
-  alteraTexto() {
-    this.title = 'Teste';
+  markAsDone(todo: Todo){
+    todo.done = true;
+    this.save();
+  }
+
+  markAsUndone(todo: Todo){
+    todo.done = false;
+    this.save();
+  }
+
+  save(){
+    const data =JSON.stringify(this.todos);
+    localStorage.setItem('todos', data);
+  }
+
+  load(){
+    const data= localStorage.getItem('todos');
+    this.todos = JSON.parse(data!);
   }
 }
